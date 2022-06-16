813213333e9c872f82ea210ac4023e6c5551f55d
opara nnabueze <villavelle101@gmail.com>
2022-06-16 17:46:17 +0100

    fixing

--- main.tf
+++ main.tf
@@ -4,11 +4,6 @@
 resource "aws_s3_bucket" "backend" {
   bucket = var.app-name
   acl    = "private"
-  force_destroy = true
-
-  versioning {
-    enabled = true
-  }
 
   tags = {
     Name        = var.app-name
